433e8914cee410d974c05bf8d6787f195046fa8f
gottox@rootkit.lan <gottox@rootkit.lan>
2007-12-19 00:16:32 +0100

    initial checkin

---
+++ config.mk
@@ -0,0 +1,19 @@
+# tar version
+VERSION = 0.0
+
+# Customize below to fit your system
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/share/man
+
+# includes and libs
+INCS = -I. -I/usr/include
+LIBS = -L/usr/lib -lc
+
+# flags
+CFLAGS = -static -Os -g -Wall -Werror ${INCS} -DVERSION=\"${VERSION}\"
+LDFLAGS = -g ${LIBS}
+
+# compiler and linker
+CC = cc
